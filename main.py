77c15751e2a8026ef93abf2b87e83bb87c5deed2
lcorelli <leonardcorelli@gmail.com>
2022-06-08 14:06:24 -0400

    adding  more features to makefile

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import fire
 
 
 def sentiment_analysis(text: str):
-    model = pipeline("text-classification",model="distilbert-base-uncased-finetuned-sst-2-english")
+    model = pipeline(
+        "text-classification", model="distilbert-base-uncased-finetuned-sst-2-english"
+    )
     return model(text)
 
 
